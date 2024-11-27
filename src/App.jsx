1b79d44823e3d5babcc9458f075c591e6d731693
NinaKharitonova <ninakharitonova95@email.com>
2024-11-27 16:36:31 +0100

    Struttura iniziale esercizio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,102 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {}
+function App() {
+  const tasks = [
+    {
+      id: 1,
+      title: "Implementare la homepage",
+      priority: 1,
+      estimatedTime: 120,
+      state: "completed",
+    },
+    {
+      id: 2,
+      title: "Sviluppare il menu di navigazione",
+      priority: 2,
+      estimatedTime: 60,
+      state: "completed",
+    },
+    {
+      id: 3,
+      title: "Creare il footer del sito",
+      priority: 3,
+      estimatedTime: 30,
+      state: "completed",
+    },
+    {
+      id: 4,
+      title: "Ottimizzare le performance",
+      priority: 1,
+      estimatedTime: 180,
+      state: "completed",
+    },
+    {
+      id: 5,
+      title: "Scrivere test per i componenti",
+      priority: 2,
+      estimatedTime: 90,
+      state: "completed",
+    },
+    {
+      id: 6,
+      title: "Implementare la pagina dei contatti",
+      priority: 3,
+      estimatedTime: 60,
+      state: "completed",
+    },
+    {
+      id: 7,
+      title: "Aggiungere animazioni CSS",
+      priority: 2,
+      estimatedTime: 30,
+      state: "backlog",
+    },
+    {
+      id: 8,
+      title: "Integrare l'API di autenticazione",
+      priority: 1,
+      estimatedTime: 120,
+      state: "in_progress",
+    },
+    {
+      id: 9,
+      title: "Aggiornare la documentazione",
+      priority: 3,
+      estimatedTime: 60,
+      state: "backlog",
+    },
+    {
+      id: 10,
+      title: "Rifattorizzare il codice CSS",
+      priority: 2,
+      estimatedTime: 90,
+      state: "in_progress",
+    },
+  ];
+  console.log(tasks);
+
+  const backlogAndInProgres = () => {
+    return tasks.filter(
+      (task) => task.state === "backlog" || task.state === "in_progress"
+    );
+  };
+
+  const completedTasks = () => {
+    return tasks.filter((task) => task.state === "completed");
+  };
+
+  return (
+    <>
+      <h1>Task Manager</h1>
+
+      <h2>In Progress / Backlog</h2>
+      <ul key={backlogAndInProgres}></ul>
+
+      <h2>Completed</h2>
+      <ul key={completedTasks}></ul>
+    </>
+  );
+}
 
 export default App;
